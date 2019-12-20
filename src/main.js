9ec2f3d9b212350dfd4184c976461f3f5e6989a7
the-helloworld <1404081357@qq.com>
2019-12-20 16:48:56 +0800

    zyp 二日晚1.1

--- src/main.js
+++ src/main.js
@@ -4,21 +4,6 @@ import router from './router'
 import "../static/js/rem"
 import Vant from 'vant';
 import 'vant/lib/index.css';
-<<<<<<< HEAD
-
-
-Vue.config.productionTip = false
-Vue.use(Vant);
-/* eslint-disable no-new */
-
-
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.config.productionTip = false
-
-
-Vue.use(ElementUI)
-=======
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -31,7 +16,6 @@ Vue.use(ElementUI);
 Vue.config.productionTip = false
 
 
->>>>>>> ace217a2b93f032e02ccf00f6b020dbbd8ec7ac7
 new Vue({
   el: '#app',
   router,
