641ca4183e8f1be1ce19e5ba97cadf864e0f38cb
the-helloworld <1404081357@qq.com>
2019-12-20 11:51:54 +0800

    zyp 路由跳转实现图片切换1.2 优化版本

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,49 +1,10 @@
 <template>
     <footer>
-        <!-- <ul>
-            <router-link to="/show" tag="li">
-                <span>
-                    <img src="/static/img/shouye.png" alt="" v-show="!this.show">
-                    <img src="/static/img/shouye02.png" alt="" v-show="this.show">
-                </span>
-                <p>首页</p>
-            </router-link> 
-            <router-link to="/ke" tag="li">
-                <span>
-                    <img src="/static/img/kechengpng.png" alt="" v-show="!this.show">
-                    <img src="/static/img/kecheng02.png" alt="" v-show="this.show">
-                </span>
-                <p>课程</p>
-            </router-link> 
-            <router-link to="/yue" tag="li">
-                <span>
-                    <img src="/static/img/yueke.png" alt="" v-show="!this.show">
-                    <img src="/static/img/yueke02.png" alt="" v-show="this.show">
-                </span>
-                <p>约课记录</p>
-            </router-link> 
-            <router-link to="/li" tag="li">
-                <span>
-                    <img src="/static/img/lianxi.png" alt="" v-show="!this.show">
-                    <img src="/static/img/lianxi02.png" alt="" v-show="this.show">
-                </span>
-                <p>练习</p>
-            </router-link> 
-            <router-link to="/wo" tag="li" >
-                <span>
-                    <img src="/static/img/wode.png" alt="" v-show="!this.show">
-                    <img src="/static/img/lianxi02.png" alt="" v-show="this.show">
-                </span>
-                <p>我的</p>
-            </router-link> 
-            </ul>
-             -->
-        <ul>
+       
+        <ul>                                          
               <router-link v-for="(v,i) in list" :to="v.src" tag="li" :key="i"  >
                 <span>
                     <img :src="v.img">
-                    <!-- <img :src="v.img2" alt="" > -->
-                    <!-- <img src="/static/img/lianxi02.png" alt="" v-show="this.show"> -->
                 </span>
                 <p>{{v.title}}</p>
             </router-link> 
@@ -60,7 +21,7 @@ export default {
                {title:"首页",src:"/show",img:"/static/img/shouye.png",zt:true,img2:"/static/img/shouye02.png"},
                {title:"课程",src:"/ke",img:"/static/img/kechengpng.png",zt:true,img2:"/static/img/kecheng02.png"},
                {title:"约课记录",src:"/yue",img:"/static/img/yueke.png",zt:true,img2:"/static/img/yueke02.png"},
-               {title:"我的",src:"/li",img:"/static/img/lianxi.png",zt:true,img2:"/static/img/lianxi02.png"},
+               {title:"课程",src:"/li",img:"/static/img/lianxi.png",zt:true,img2:"/static/img/lianxi02.png"},
                {title:"我的",src:"/wo",img:"/static/img/wode.png",zt:true,img2:"/static/img/lianxi02.png"}
             ]
         }
@@ -70,10 +31,10 @@ export default {
         this.list.forEach(data=>{
            if(this.$route.path==data.src){
                console.log(data.img="/static/img"+data.src+"02.png")
-            //    data.img=data.img="/static/img"+data.src+"02.png"
                data.img=data.img2
            }
         })
+       
        
     }
 }
